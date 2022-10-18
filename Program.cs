0c899de0f0932e06f55e3ee8e6c22fb92bc2b9b3
Kenzodiac <kennethfujimura@gmail.com>
2022-10-18 07:09:56 -0700

    Mild revisions and clean up

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿//Kenneth Fujimura
-//09-12-2022 @10:08pm
+//10-18-2022
 //Mini Challenge # 4 - Greater Than Or Less Than
 //Console program greets the user, asks for input of two different numbers, proceses those string values into intergers, ...
 //...compares which value is larger, and then reports that information to the user.
-//Peer Review by: see mini challenge #1
+//Peer Review by:
 
 Console.Clear();
 Console.WriteLine("Let's add two numbers!");
@@ -13,25 +13,20 @@ bool playAgain = true;
 //play again loop
 while (playAgain == true) {
     //declaring variables for various functions
-    string stringNum1 = "";
-    string stringNum2 = "";
-
     bool isConverted1 = false;
-    bool isNumber1;
     int intNum1 = 0;
 
     bool isConverted2 = false;
-    bool isNumber2;
     int intNum2 = 0;
 
     //taking first number input, with data validation
     Console.WriteLine("Welcome! This program will tell you if two numbers are greater than, less than, or equal to each other.");
     Console.WriteLine("On the keyboard, please input one number: ");
     while (!isConverted1) {
-        stringNum1 = Console.ReadLine();
-        isNumber1 = Int32.TryParse(stringNum1, out intNum1);
+        string stringNum = Console.ReadLine();
+        bool isNumber = Int32.TryParse(stringNum, out intNum1);
 
-        if (isNumber1 == true){
+        if (isNumber == true){
             isConverted1 = true;
         } else {
             Console.WriteLine("Invalid entry. Please enter a whole number or 'Integer'.");
@@ -41,10 +36,10 @@ while (playAgain == true) {
     //taking second number input, with data validation
     Console.WriteLine("Great! Now, please input the second number: ");
     while (!isConverted2) {
-        stringNum2 = Console.ReadLine();
-        isNumber2 = Int32.TryParse(stringNum2, out intNum2);
+        string stringNum = Console.ReadLine();
+        bool isNumber = Int32.TryParse(stringNum, out intNum2);
 
-        if (isNumber2 == true){
+        if (isNumber == true){
             isConverted2 = true;
         } else {
             Console.WriteLine("Invalid entry. Please enter a whole number or 'Integer'.");
@@ -55,33 +50,39 @@ while (playAgain == true) {
     //int intNum2 = Convert.ToInt32(stringNum2);
 
     //number comparisons
-    if(intNum1 > intNum2){
-        Console.WriteLine("Your first number \"" + stringNum1 + "\" is GREATER THAN your second number \"" + stringNum2 + "\"");
-    }else if(intNum1 < intNum2){
-        Console.WriteLine("Your first number \"" + stringNum1 + "\" is LESS THAN your second number \"" + stringNum2 + "\"");
-    }else{
-        Console.WriteLine("Your first number \"" + stringNum1 + "\" is EQUAL TO your second number \"" + stringNum2 + "\"");
-    }
+    NumberComp(intNum1, intNum2);
 
-    Console.WriteLine("Also...!");
+    //method for doing number comparisons
+    void NumberComp(int num1, int num2){
+        
+        if(num1 > num2){
+            Console.WriteLine("Your first number \"" + num1 + "\" is GREATER THAN your second number \"" + num2 + "\"");
+        }else if(intNum1 < intNum2){
+            Console.WriteLine("Your first number \"" + num1 + "\" is LESS THAN your second number \"" + num2 + "\"");
+        }else{
+            Console.WriteLine("Your first number \"" + num1 + "\" is EQUAL TO your second number \"" + num2 + "\"");
+        }
 
-    if(intNum2 > intNum1){
-        Console.WriteLine("Your second number \"" + stringNum2 + "\" is GREATER THAN your first number \"" + stringNum1 + "\"");
-    }else if(intNum2 < intNum1){
-        Console.WriteLine("Your second number \"" + stringNum2 + "\" is LESS THAN your first number \"" + stringNum1 + "\"");
-    }else{
-        Console.WriteLine("Your second number \"" + stringNum2 + "\" is EQUAL TO your first number \"" + stringNum1 + "\"");
+        Console.WriteLine("Also...!");
+
+        if(num2 > num1){
+            Console.WriteLine("Your second number \"" + num2 + "\" is GREATER THAN your first number \"" + num1 + "\"");
+        }else if(intNum2 < intNum1){
+            Console.WriteLine("Your second number \"" + num2 + "\" is LESS THAN your first number \"" + num1 + "\"");
+        }else{
+            Console.WriteLine("Your second number \"" + num2 + "\" is EQUAL TO your first number \"" + num1 + "\"");
+        }
     }
 
     //play again prompt with input validation
-    Console.WriteLine("Would you like to compare another pair of numbers? Y/N");
+    Console.Write("Would you like to compare another pair of numbers? Y/N: ");
     bool answerCheck = true;
     while (answerCheck == true) {
-        string playAgainInput = Console.ReadLine();
-        if (playAgainInput == "y" || playAgainInput == "Y") {
+        string playAgainInput = Console.ReadLine().ToLower();
+        if (playAgainInput == "y" || playAgainInput == "yes") {
             answerCheck = false;
             continue;
-        } else if (playAgainInput == "n" || playAgainInput == "N"){
+        } else if (playAgainInput == "n" || playAgainInput == "no"){
             playAgain = false;
             answerCheck = false;
         } else {
